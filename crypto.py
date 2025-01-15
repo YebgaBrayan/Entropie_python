b10e6ee89c9072e485e775ea4913843a9cb64223
YebgaBrayan <95120191+YebgaBrayan@users.noreply.github.com>
2025-01-15 03:44:33 +0100

    création  et importation
    création du projet et importation des bibliothèques

---
+++ crypto.py
@@ -0,0 +1,10 @@
+import cv2
+import hashlib
+import os
+import requests
+import argparse
+import mysql.connector
+from mysql.connector import errorcode
+from PIL import Image, ImageFilter
+import sys
+

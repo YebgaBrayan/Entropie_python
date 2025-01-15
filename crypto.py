2e36647f5ce910d93008aefe71228a15015b2468
Yebga brayan <yebgabrayan@gmail.com>
2025-01-15 04:04:46 +0100

    ajout d'une fonction pour telecharger l'image depuis internet

--- crypto.py
+++ crypto.py
@@ -8,3 +8,14 @@ from mysql.connector import errorcode
 from PIL import Image, ImageFilter
 import sys
 
+def telecharger_image(url, filepath, timeout):
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        with open(filepath, 'wb') as image_file:
+            image_file.write(response.content)
+        print(f"Image téléchargée et sauvegardée sous : {filepath}")
+        return filepath
+    except requests.RequestException as e:
+        print(f"Erreur lors du téléchargement de l'image : {e}")
+        return None
